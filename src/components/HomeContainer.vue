7b21b37429315500cb44fb6c5ea3dcf02c948f66
evanGRU <evangruchot.pro@gmail.com>
2023-09-29 11:49:58 +0200

    add homepage + global code + routes

---
+++ src/components/HomeContainer.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps(['title', 'datas']);
+</script>
+
+<template>
+  <div>
+    <h1 class="container-title">{{ title }}</h1>
+    <div class="container d-flex">
+      <div v-for="data in props.datas" class="card">
+        <h3>{{ data }}</h3>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container-title{
+  border-bottom: #fff 2px solid;
+}
+.container{
+  width: 100%;
+  gap: 1em;
+  justify-content: space-between;
+}
+
+.card{
+  background-color: #757575;
+  width: 20em;
+  padding: 0em 1em;
+}
+</style>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,15 @@
+<script setup>
+import HomeContainer from "../components/HomeContainer.vue";
+
+const moviesList = ['Movie 1', 'Movie 2', 'Movie 3', 'Movie 4'];
+const actorsList = ['Actor 1', 'Actor 2', 'Actor 3', 'Actor 4'];
+
+</script>
+
+<template>
+  <HomeContainer title="Movies" :datas="moviesList"/>
+  <HomeContainer title="Actors" :datas="actorsList"/>
+</template>
+
+<style scoped>
+</style>
